024d859572545dfda2ffd3ff616ff7f7889fa2ac
Karthik.Shekar <karthik.shekar@yahoo.com>
2025-02-26 12:59:49 +1100

    region updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "terraformstateforrcp"
     key            = "rcp/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraformrcplocktable"
     encrypt        = true
   }
